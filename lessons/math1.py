233b12826b2684900633ba29b66cf2c81dadb05b
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-06-08 21:24:43 +0300

    add math lessons

---
+++ lessons/math1.py
@@ -0,0 +1,45 @@
+# ПЕЧАТЬ ЗНАКОВ ПОСЛЕ ЗАПЯТОЙ 
+# from math import e # 2.718281828459045
+# print(f'{e:.2f}') # 2.72
+
+
+# Поиск Max и Min числа  
+# x = int(input('Введите целое число x: ')) 
+# y = int(input('Введите целое число y: '))
+# 1.
+# if y > x:
+	# Max, Min = y, x
+# else:
+	# Max, Min = x, y
+# print(f'Max: {Max}\nMin: {Min}')
+# 2.
+# print(f'Max: {x}\nMin: {y}' if x > y else f'Max: {y}\nMin: {x}')
+
+
+# print('СУММА ПЕРВЫХ n НАТУРАЛЬНЫХ ЧИСЕЛ')   
+# n = int(input('Введите целое число n: '))
+# result = 0 
+# for i in range(1, n+1):
+	# result += i
+# print(result)
+
+
+# def greeting(name:str):
+# 	''' Приветствие, name - Ваше имя'''
+# 	return f'Hello {name}'
+# print(greeting('Andrey'))
+
+
+# from math import sqrt
+# def geron(a:int, b:int, c:int):
+# 	'''Вычисление площади треугольника по формуле Герона\na, b, c - длины сторон
+# 	'''
+# 	p = (a + b + c) / 2
+# 	area = sqrt(p * (p - a) * (p - b) * (p - c))
+# 	return area
+# print(f'{geron(6, 7, 5):.1f}')
+	
+
+
+
+
